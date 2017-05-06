e3c4dddd9d6b8c66c9b6ff54e71187c711139afe
Salvatore Testa <sal@squareup.com>
2017-05-05 19:13:48 -0700

    Create a sample app with OOM error

---
+++ leakcanary-deadcanary/src/main/java/com/example/deadcanary/MainActivity.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2017 Square, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.deadcanary;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main_activity);
+
+    View button = findViewById(R.id.async_task);
+    button.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View v) {
+        (new Crasher()).execute();
+      }
+    });
+  }
+
+  static class Crasher extends AsyncTask<Void,Void,Void> {
+    @Override protected Void doInBackground(Void... params) {
+      int[] memoryFillIntVar = new int[Integer.MAX_VALUE];
+      return null;
+    }
+  }
+}
+
+
